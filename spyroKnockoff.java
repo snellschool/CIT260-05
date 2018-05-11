e8328ee7e016ec59f1d244071a8ad447bcaa25cb
Dallin Snell <38664409+snellschool@users.noreply.github.com>
2018-05-11 13:26:37 -0600

    Create spyroKnockoff.java

---
+++ spyroKnockoff.java
@@ -0,0 +1,18 @@
+/* Snell, Dallin
+** 30 APR 18
+** We can come up with a better title a bit later. :)
+*/
+
+
+
+public class spyroKnockoff {
+  /**
+   * @param args the arguments passed in from the command line
+   * @return exit code
+   */
+  public static void main (String[] args) {
+    System.out.println("Welcome to the Spyro Knockoff game!");
+    
+    return 0;
+  } // end of main()
+} // end of class
