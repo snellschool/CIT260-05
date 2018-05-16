b283942cad63a4629162bfb7365e7d1ade482030
Dallin Snell <dscarman17@gmail.com>
2018-05-16 13:02:42 -0600

    Changed debugging inputs.

--- spyroKnockoff/src/spyroknockoff/SpyroKnockoff.java
+++ spyroKnockoff/src/spyroknockoff/SpyroKnockoff.java
@@ -16,14 +16,14 @@ public class SpyroKnockoff {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Game play = new Game(42);                   // debugging
-        Area area = new Area();                     // debugging
-        Actor act = new Actor(null, null);          // debugging
-        Inventory inven = new Inventory(42, 42);    // debugging
-        Location loc = new Location(2, 5);          // debugging
-        Map map = new Map();                        // debugging
-        Player player = new Player("Megan", 12345); // debugging
-        Riddle ridd = new Riddle();                 // debugging
+        Game play = new Game(42);                       // debugging
+        Area area = new Area();                         // debugging
+        Actor act = new Actor(null, null);              // debugging
+        Inventory inven = new Inventory(42, 42);        // debugging
+        Location loc = new Location(2, 5);              // debugging
+        Map map = new Map();                            // debugging
+        Player player = new Player("Steven", 12345);    // debugging
+        Riddle ridd = new Riddle();                     // debugging
         
         System.out.println("Game hash: " + play.hashCode());    // debugging
         System.out.println("Area hash: " + area.hashCode());    // debugging
