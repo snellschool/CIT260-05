8755369036ed2f6cfe9fc091f3bc9250a947e268
Dallin Snell <dscarman17@gmail.com>
2018-06-01 13:11:55 -0600

    * added riddleMathThird() algorithm * added riddleMathThird() test cases

--- spyroKnockoff/src/byui/cit260/control/RiddleControl.java
+++ spyroKnockoff/src/byui/cit260/control/RiddleControl.java
@@ -40,14 +40,12 @@ public class riddleControl {
             }
 
     return (((5 * xVal) + (3 * yVal) - 7) == (-7)) ? 1 : 0;
-//END
     } // end of riddleMathFirst()
     
     /**
      * second math problem 
-     * author Megan
+     * @author Megan
      */
-    
     public static int riddleMathSecond(int choiceOne, int choiceTwo) {
         int xVal = 0;
         int yVal = 0;
@@ -81,4 +79,35 @@ public class riddleControl {
 	
     } // end of riddleMathSecond ()
     
+    public static int riddleMathThird(int choiceOne, int choiceTwo) {
+        int xVal = 0;
+        int yVal = 0;
+        
+        if((choiceOne <= 0 || choiceOne >= 5) || (choiceTwo <= 0 || choiceTwo >= 5))
+            return -1; // if the user entered invalid choices
+        
+            switch(choiceOne) {
+                case 1: xVal = 30;
+                    break;
+                case 2: xVal = 3;
+                    break;
+                case 3: xVal = 2;
+                    break;
+                case 4: xVal = 42;
+                    break;
+            }
+
+            switch(choiceTwo) {
+                case 1: yVal = 0;
+                    break;
+                case 2: yVal = 42;
+                    break;
+                case 3: yVal = -9;
+                    break;
+                case 4: yVal = -7;
+                    break;
+            }
+
+    return ((7 * xVal) + (5 * yVal) + 24 == 0) ? 1 : 0;
+    } // end of riddleMathThird()
 } // end of class RiddleControl
